858ea863ca1c40cc5b9d030d7ce453a5db15d5a5
kanderson425 <kyleanderson.dev@gmail.com>
2020-04-20 18:04:58 -0500

    Adds the lesson notes from the __name__ and __main__ lesson.

---
+++ 06-Modules and Packages/01-Name_and_Main/one_practice.py
@@ -0,0 +1,16 @@
+#one_practice.py
+
+def func():
+	print("FUNC() IN ONE_PRACTICE.PY")
+
+print("TOP LEVEL IN ONE_PRACTICE.PY")
+
+def function():
+	pass
+
+def function2():
+	pass
+
+if __name__ == '__main__':
+	function2()
+	function()
---
+++ 06-Modules and Packages/01-Name_and_Main/two_practice.py
@@ -0,0 +1,11 @@
+#two_practice.py
+import one
+
+print("TOPY LEVEL IN two_practice.PY")
+
+one.func()
+
+if __name__ == '__main__':
+	print("TWO_PRACTICE.PY is being run directly")
+else:
+	print("TWO_PRACTICE.PY has been imported!")
