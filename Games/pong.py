4591bfc0c5bf0a779b1f10b16a3343b961222247
kanderson425 <kyleanderson.dev@gmail.com>
2020-05-01 14:40:52 -0500

    Adds the logic underlying ball moving out of frame on x-axis.

--- Games/pong.py
+++ Games/pong.py
@@ -33,6 +33,8 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0,0)
+ball.dx = 2
+ball.dy = -2
 
 # Function 
 def paddle_a_up():
@@ -45,16 +47,53 @@ def paddle_a_down():
     y -= 20
     paddle_a.sety(y)
 
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
 # Keyboard binding
 wn.listen()
 wn.onkey(paddle_a_up, "w")
 wn.onkey(paddle_a_down,"s")
+wn.onkey(paddle_b_up, "Up")
+wn.onkey(paddle_b_down,"Down")
 
     
 
 # Need a main Game Loop
 while True:
     wn.update()
+
+    #Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border Checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0,0)
+        ball.dx *= -1
+
+    if ball.xcor() < -390:
+        ball.goto(0,0)
+        ball.dx *= -1
+
+    # Paddle and Ball Collisions
+
+        
 
 
 
