78045ad26ca9c943cc8a33837d336cdbbe9ee398
kanderson425 <kyleanderson.dev@gmail.com>
2020-05-22 23:17:27 -0500

    Adds the move_left and move_right functions to the space invaders game.

--- Games/space-invaders.py
+++ Games/space-invaders.py
@@ -13,11 +13,27 @@ border_pen.speed(0)
 border_pen.color("white")
 border_pen.penup()
 border_pen.setposition(-300,-300)
+border_pen.pendown()
 border_pen.pensize(3)
 for side in range(4):
     border_pen.fd(600)
     border_pen.lt(90)
 border_pen.hideturtle()
+
+#Create the player turtle
+player = turtle.Turtle()
+player.color("blue")
+player.shape("triangle")
+player.penup()
+player.speed(0)
+player.setposition(0, - 250)
+player.setheading(90)
+
+
+
+
+
+
 
 delay = input("Press enter to finish.")
 
