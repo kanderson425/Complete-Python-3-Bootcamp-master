743f1c30f42cf455706254a780e6ccc56fd8ed41
kanderson425 <kyleanderson.dev@gmail.com>
2020-05-26 23:50:56 -0500

    Adds the go_left and go_right methods to the falling skies game.

--- Games/falling-skies/falling-skies.py
+++ Games/falling-skies/falling-skies.py
@@ -8,5 +8,37 @@ wn.title("Falling Skies by @TokyoEdTech")
 wn.bgcolor("green")
 wn.setup(width=800, height=600)
 
+#Add the player
+player = turtle.Turtle()
+player.speed(0)
+player.shape("square")
+player.color("white")
+player.penup()
+player.goto(0, -250)
+player.direction = "stop"
 
-raw_input = input("Press enter to clear out.")
+# Functions
+def go_left():
+    player.direction = "left"
+
+def go_right():
+    player.direction = "right"
+
+# Main game loop
+while True:
+
+    # Move the player
+    if player.direction == "left":
+        x = player.xcor()
+        x -= 3
+        player.setx(x)
+
+    if player.direction == "right":
+        x = player.xcor()
+        x += 3
+        player.setx(x)
+
+
+
+
+wn.mainloop()
